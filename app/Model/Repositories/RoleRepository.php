40017a4d204726d666193e9880b22eab3c5b0639
ludd00 <ludd00@vse.cz>
2022-01-13 00:05:40 +0100

    Přidání správy uživatelů (úprava jména a role)

--- app/Model/Repositories/RoleRepository.php
+++ app/Model/Repositories/RoleRepository.php
@@ -8,4 +8,16 @@ namespace App\Model\Repositories;
  */
 class RoleRepository extends BaseRepository{
 
+  public function getRole($id) {
+    $row = $this->connection->select('*')
+      ->from($this->getTable())
+      ->where($this->mapper->getPrimaryKey($this->getTable()) . '= %s', $id)
+      ->fetch();
+
+    if (!$row) {
+      throw new \Exception('Entity was not found.');
+    }
+    return $this->createEntity($row);
+  }
+
 }
