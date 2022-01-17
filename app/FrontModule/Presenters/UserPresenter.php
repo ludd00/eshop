35ade0b0a96197196bfe7a45015c48ef4bd6eeb8
ludd00 <ludd00@vse.cz>
2022-01-17 16:13:25 +0100

    uprava detailu produktu

--- app/FrontModule/Presenters/UserPresenter.php
+++ app/FrontModule/Presenters/UserPresenter.php
@@ -187,6 +187,7 @@ class UserPresenter extends BasePresenter{
    * Formulář pro obnovu zapomenutého hesla
    * @return ForgottenPasswordForm
    *
+   */
   protected function createComponentForgottenPasswordForm():ForgottenPasswordForm{
     $form=$this->forgottenPasswordFormFactory->create();
     $form->onFinished[]=function($message=''){
